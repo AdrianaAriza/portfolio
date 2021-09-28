7cf5e5e853ddd7ebe1cda8b7f33a943437f94a02
AdrianaAriza <a-d-r-u@hotmail.com>
2021-09-27 22:46:22 -0400

    [Feature]: Contactt me view

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -8,7 +8,6 @@
 
 // Function for the submit form button
 
-  'use strict'
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
   
@@ -20,8 +19,24 @@
           event.preventDefault()
           event.stopPropagation()
         }
-  
         form.classList.add('was-validated')
       }, false)
     })
 })()
+
+// captturing user information and saving in local storage
+function get_data(){
+  console.log('getting data from user')
+  var firstName = document.getElementById('validationTooltip01').value
+  var lastName = document.getElementById('validationTooltip02').value
+  var email = document.getElementById('validationTooltip03').value
+  var number = document.getElementById('validationTooltip04').value
+  var subject = document.getElementById('validationTooltip05').value
+  var message = document.getElementById('validationTooltip06').value
+  localStorage.setItem("name", firstName)
+  localStorage.setItem("lastname", lastName)
+  localStorage.setItem("email", email)
+  localStorage.setItem("number", number)
+  localStorage.setItem("subject", subject)
+  localStorage.setItem("message", message)
+}
